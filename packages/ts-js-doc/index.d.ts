dcde0a96cf572a6b2ee4b7c9ff2c7d7bd14aec7c
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-06-01 00:02:41 +0300

    remove dist

--- packages/ts-js-doc/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as ts from 'typescript';
-export declare class JsDoc {
-    static description(node: ts.Node): string;
-    static comment(node: ts.Node, tagName: string): string;
-    static tags(node: ts.Node, isMatching: (tag: ts.JSDocTag) => boolean): ts.JSDocTag[];
-    static hasTag(node: ts.Node, isMatching: (tag: ts.JSDocTag) => boolean): boolean;
-    static getJSDocTagNames(node: ts.Node): string[];
-}
