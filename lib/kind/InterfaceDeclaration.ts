88305e57a60f2d0f961aff2570bccb7ecb24d416
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-06-23 19:24:41 +0300

    implement interface resolver

--- lib/kind/InterfaceDeclaration.ts
+++ lib/kind/InterfaceDeclaration.ts
@@ -1,2 +1,32 @@
+import * as ts from 'typescript';
+import {TypeResolver} from '../TypeResolver';
 
-// TODO: implement
+export class InterfaceDeclaration {
+    private typeResolver: TypeResolver;
+
+    constructor(typeResolver: TypeResolver) {
+        this.typeResolver = typeResolver;
+    }
+
+    isSupport(type: ts.TypeNode): boolean {
+        return type.kind === ts.SyntaxKind.InterfaceDeclaration;
+    }
+
+    resolve(type: ts.InterfaceDeclaration) {
+        const properties: {[key: string]: any} = {};
+
+        for (const property of type.members.filter(ts.isPropertySignature)) {
+            const resolvedType: any = this.typeResolver.resolve(property.type);
+            const name: string = property.name.getText();
+
+            if (name) {
+                properties[name] = resolvedType;
+            }
+        }
+
+        return {
+            type: 'object',
+            properties,
+        };
+    }
+}
