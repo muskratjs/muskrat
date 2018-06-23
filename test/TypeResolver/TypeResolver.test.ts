de36784acf8d858308b1204c31ca51fd3bd70619
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-06-23 23:38:24 +0300

    optimize tests

--- test/TypeResolver/TypeResolver.test.ts
+++ test/TypeResolver/TypeResolver.test.ts
@@ -9,44 +9,61 @@ import * as kindResolvers from '../../lib/kind';
 import {TypeResolver} from '../../lib/TypeResolver';
 
 const assert = require('chai').assert;
+const isTested: string[] = [];
+const nodes: ts.TypeNode[] = [];
 const compilerOptions = require(path.join(process.cwd(), 'tsconfig.json'));
+const testFoldersPattern = path.join(process.cwd(), 'test/TypeResolver/resources/*');
+const program = ts.createProgram(
+    glob.sync(testFoldersPattern)
+        .map((testPath) => path.join(testPath, 'main'))
+    ,
+    compilerOptions || {}
+);
+const typeChecker = program.getTypeChecker();
+
+program.getSourceFiles()
+    .filter((sourceFile) => minimatch(sourceFile.fileName, `${testFoldersPattern}*`))
+    .forEach((sourceFile) => {
+        ts.forEachChild(sourceFile, (n: ts.TypeNode) => {
+            nodes.push(n);
+        });
+    })
+;
+
+const typeResolver = new TypeResolver(nodes, typeChecker);
+
+for (const resolver of values(kindResolvers)) {
+    typeResolver.register(resolver);
+}
+
+// const node = nodes.filter(n => n.kind !== ts.SyntaxKind.EndOfFileToken).shift();
+//
 
 describe('type-resolver', () => {
-    glob.sync(path.join(__dirname, 'resources/*')).forEach((testFolder: string) => {
-        it(path.basename(testFolder), () => {
-            const nodes: ts.TypeNode[] = [];
-            const schema = JSON.parse(
-                fs.readFileSync(
-                    path.join(process.cwd(),
-                        'test/TypeResolver/resources',
-                        path.basename(testFolder),
-                        'schema.json'
-                    )
-                ).toString()
-            );
-            const program = ts.createProgram([path.join(testFolder, 'main')], compilerOptions || {});
-            const typeChecker = program.getTypeChecker();
-            const folder = path.join(__dirname, 'resources', path.basename(testFolder));
+    nodes
+        .filter(n => n.kind !== ts.SyntaxKind.EndOfFileToken)
+        .forEach((node: ts.TypeNode) => {
+            const testFilePath = node.getSourceFile().fileName;
+            const testFolder = path.basename(path.dirname(node.getSourceFile().fileName))
 
-            program.getSourceFiles()
-                .filter((sourceFile) => minimatch(sourceFile.fileName, `${folder}/*`))
-                .forEach((sourceFile) => {
-                    ts.forEachChild(sourceFile, (n: ts.TypeNode) => {
-                        nodes.push(n);
-                    });
-                })
-            ;
+            if (isTested.indexOf(testFilePath) === -1) {
+                isTested.push(testFilePath);
 
-            const typeResolver = new TypeResolver(nodes, typeChecker);
+                it(testFolder, () => {
+                    const schema = JSON.parse(
+                        fs.readFileSync(
+                            path.join(process.cwd(),
+                                'test/TypeResolver/resources',
+                                path.basename(testFolder),
+                                'schema.json'
+                            )
+                        ).toString()
+                    );
 
-            for (const resolver of values(kindResolvers)) {
-                typeResolver.register(resolver);
+                    const resolvedSchema = typeResolver.resolve(node);
+
+                    assert.deepEqual(resolvedSchema, schema);
+                });
             }
-
-            const node = nodes.filter(n => n.kind !== ts.SyntaxKind.EndOfFileToken).shift();
-            const resolvedSchema = typeResolver.resolve(node);
-
-            assert.deepEqual(resolvedSchema, schema);
         });
-    });
 });
--- test/TypeResolver/resources/enums-member/main.ts
+++ test/TypeResolver/resources/enums-member/main.ts
@@ -1,9 +1,9 @@
+export interface MyObject {
+    code: A.B;
+}
+
 enum A {
   B,
   C,
   D,
-}
-
-export interface MyObject {
-  code: A.B;
 }
