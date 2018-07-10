29781cfec014c6392c712e776e917687b8ec0477
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-07-11 01:54:38 +0300

    add format for string type

--- lib/model/type/StringType.ts
+++ lib/model/type/StringType.ts
@@ -1,6 +1,10 @@
 import {PrimitiveType} from './PrimitiveType';
 
 export class StringType extends PrimitiveType {
+    constructor(private format?: string) {
+        super();
+    }
+
     public getId(): string {
         return 'string';
     }
