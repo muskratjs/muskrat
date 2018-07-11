86de5d14731570fd3cc8fe7d23cf71fc0495a9e8
Igor Ognichenko <igor.ognichenko@fireblink.com>
2018-07-11 11:57:07 +0300

    update pet model

--- test/MetadataGenerator/models/PetModel.ts
+++ test/MetadataGenerator/models/PetModel.ts
@@ -1,11 +1,17 @@
-export class PetModel {
+import {IsInt, IsString, Length, Min} from 'class-validator';
 
+export class PetModel {
+    @IsInt()
+    @Min(1)
     id: number;
 
+    @Length(10, 20)
     fullName?: string;
 
+    @IsString()
     type = 'Panda';
 
+    @IsString()
     color = 'White';
 
     constructor(public name = 'Austin') {
