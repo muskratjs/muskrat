8e1bc94f9bb0c9e142e4509cb50d385b28093abb
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-07-15 01:02:00 +0300

    remove unused exceptions

--- lib/exception/index.ts
+++ lib/exception/index.ts
@@ -1,7 +1,5 @@
 export * from './AssertionException';
 export * from './BaseException';
-export * from './DiagnosticException';
 export * from './LogicException';
-export * from './NoRootTypeException';
 export * from './UnknownNodeException';
 export * from './UnknownTypeException';
