662fda5cd084821ef709b505250b039215243676
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-07-19 07:36:02 +0300

    add type-indexed-access-object-3 test

---
+++ test/TypeResolver/valid-types/type-indexed-access-object-3/main.ts
@@ -0,0 +1,14 @@
+interface SomeParent {
+    abc: 'foo';
+}
+interface SomeObject extends SomeParent {
+    def?: 'bar';
+}
+
+const obj: SomeObject = {abc: 'foo'};
+export type MyType = typeof obj['abc'];
+
+
+const a: MyType = 'foo';
+// const b: MyType = "bar";
+// const c: MyType = undefined;
