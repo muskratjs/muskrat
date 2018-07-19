699ecff72b2aac38831a325edb8553e5c665b28f
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-07-19 21:02:57 +0300

    add type-mapped-generic test

---
+++ test/TypeResolver/valid-types/type-mapped-generic/main.ts
@@ -0,0 +1,13 @@
+export type MyObject = NullableAndPartial<SomeInterface>;
+
+type PropFoo = 123;
+
+type PropBar = 'bar';
+export interface SomeInterface {
+    foo: PropFoo;
+    bar: PropBar;
+}
+
+export type NullableAndPartial<T> = {
+    [K in keyof T]?: T[K] | null;
+};
