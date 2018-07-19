50206837e0489a17c54eb3f9af82595ed83f6ff4
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-07-19 20:37:07 +0300

    add type-keyof-object-1 test

---
+++ test/TypeResolver/valid-types/type-keyof-object-1/main.ts
@@ -0,0 +1,11 @@
+interface SomeInterface {
+    foo: 12;
+    bar: 'baz';
+}
+
+export type MyType = keyof SomeInterface;
+
+
+const a: MyType = 'foo';
+const b: MyType = 'bar';
+// const c: MyType = "unknown";
