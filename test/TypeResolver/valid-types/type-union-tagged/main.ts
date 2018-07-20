9f1fb2c654e7b1b46e1e74cb0d49c353f9c4ff80
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-07-20 07:17:15 +0300

    add type-union-tagged test

---
+++ test/TypeResolver/valid-types/type-union-tagged/main.ts
@@ -0,0 +1,17 @@
+export type Shape = Square | Rectangle | Circle;
+
+export interface Square {
+    kind: 'square';
+    size: number;
+}
+
+export interface Rectangle {
+    kind: 'rectangle';
+    width: number;
+    height: number;
+}
+
+export interface Circle {
+    kind: 'circle';
+    radius: number;
+}
